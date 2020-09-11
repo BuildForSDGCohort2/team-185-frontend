82dd02d02cc19c78dcfea53a74b0620d63cd93b6
Pmusole2 <pmusole2@gmail.com>
2020-09-11 12:13:36 +0200

    Starter code added with a dummy React Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Provider } from "react-redux";
+// import { store } from './store/store.js'
+import "./App.css";
+import Header from "./components/Navigation/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./store/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Header />
+    </Provider>
   );
 }
 
