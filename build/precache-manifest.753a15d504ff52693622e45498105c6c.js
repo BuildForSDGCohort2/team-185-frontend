f86838388400bacd5ce45e5a0a21bf0bdb052379
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2020-09-24 23:24:28 +0300

    Web server package running on Express added

---
+++ build/precache-manifest.753a15d504ff52693622e45498105c6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17b96138922bae846b998d49439f6d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17e6609efe408aed01d5",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4f3d712fc3862f09686e",
+    "url": "/static/css/main.c64af84d.chunk.css"
+  },
+  {
+    "revision": "17e6609efe408aed01d5",
+    "url": "/static/js/2.c3110491.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c3110491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3d712fc3862f09686e",
+    "url": "/static/js/main.e872c128.chunk.js"
+  },
+  {
+    "revision": "272f1b71fc4d38bc4ed5",
+    "url": "/static/js/runtime-main.9cc81cf0.js"
+  }
+]);
