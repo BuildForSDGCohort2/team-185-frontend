63a177124e38c486d1e28b7dfe6c4cfb8ef6e339
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2020-09-24 16:13:23 +0300

    Added setupProxy.js file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/api',
+    '',
     createProxyMiddleware({
       target: 'https://team-185-backend.herokuapp.com',
       changeOrigin: true,
