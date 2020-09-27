07f16a168c5ab7dcd5cb737be668d43d8e340c8a
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2020-09-27 10:14:20 +0300

    added more reducers plus the login and register modal -- update 1

---
+++ build/precache-manifest.80e9caf6d7330862f939b9e2d1cfc9d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223ff35772873238b2495f304f377ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "830d20bce222ff4b9118",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4a70d463e47b0f087814",
+    "url": "/static/css/main.c64af84d.chunk.css"
+  },
+  {
+    "revision": "830d20bce222ff4b9118",
+    "url": "/static/js/2.20ee128f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.20ee128f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a70d463e47b0f087814",
+    "url": "/static/js/main.8187610f.chunk.js"
+  },
+  {
+    "revision": "272f1b71fc4d38bc4ed5",
+    "url": "/static/js/runtime-main.9cc81cf0.js"
+  }
+]);
